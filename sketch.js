ba12dace87cc914b8c29e8346db7114deca8f879
Sherlock Holmes <sherlockholmesxp@gmail.com>
2023-01-26 07:34:44 +0800

    init

---
+++ sketch.js
@@ -0,0 +1,41 @@
+let inputtext = 'this is a random quote by me pls use thank you uwu';
+let typed = '';
+let index = 0;
+let jetbrainsmono;
+let wrongchars = 0;
+
+function preload() {
+  jetbrainsmono = loadFont('assets/jetbrains.ttf');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+  fill(255);
+  textSize(16);
+  textAlign(LEFT);
+  textWrap(WORD);
+  textFont(jetbrainsmono);
+  let inputtextw = textWidth(inputtext)
+  text(inputtext, width/2 - inputtextw/2, height/2 - 50);
+  fill(66, 135, 245);
+  text(typed, width/2 - inputtextw/2, height/2 - 50);
+  fill(255);
+  textAlign(CENTER);
+  text('Wrong Characters typed: ' + wrongchars, width/2, height/2 - 100);
+}
+
+function keyPressed() {
+  if (key == inputtext[index]) {
+    typed += key;
+    index += 1;
+  } else if (keyCode == 8) {
+    typed = typed.substring(0, typed.length - 1);
+    index -= 1;
+  } else {
+    wrongchars += 1;
+  }
+}
